4a3c6a1a854c5f4686c173440fd6a3ba58fc9060
Omofovwe Akpadaka <afovwe@gmail.com>
2024-03-21 09:11:06 -0700

    finihed about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,8 +23,38 @@ function About() {
           <img className="hidden lg:block mx-auto w-[240px] md:w-[400px] h-auto lg:mr-0 lg:ml-auto" src={mobile} alt="" style={{ maxHeight: '40vh' }} />
         </div>
       </div>
-      <div className='bg-black/40 max-w-[1300px] mx-auto rounded-xl overflow-hidden'>
+      <div className='bg-black/40 max-w-[1000px] mx-auto rounded-xl overflow-hidden'>
+          <div className='px-6 py-10'>
+            <div className=' mx-auto max-w-5xl text-center'>
+              <h2 className='block w-full gray-primary-color text-3xl sm:text-4xl'>
+                Optimize your website
+              </h2>
+              <p className='mx-auto my-4 w-full max-w-xl bg-transparent text-center font-medium leading-relaxed tracking-wide text-white' >
+                Redesign your sign 
+              </p>
+            </div>
+            <div className='mx-auto max-w-7xl z-10 grid grid-cols-1 gap-10 pt-14 sm:grid-cols-2 lg:grid-cols-3'>
+              <div className='rounded-xl border border-neutral-800 bg-neutral-900/50 p-8 text-center shadow'>
+                <h3 className='text-xl italic text-gray-200' >Customizable</h3>
+                <p className='my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-400'>
+                  ailor you landing page's look and feel from the color scheme to the font size, to design of the page
+                </p>
+              </div>
+              <div className='rounded-xl border border-neutral-800 bg-neutral-900/50 p-8 text-center shadow'>
+                <h3 className='text-xl italic text-gray-200' >Customizable</h3>
+                <p className='my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-400'>
+                  ailor you landing page's look and feel from the color scheme to the font size, to design of the page
+                </p>
+              </div>
+              <div className='rounded-xl border border-neutral-800 bg-neutral-900/50 p-8 text-center shadow'>
+                <h3 className='text-xl italic text-gray-200' >Customizable</h3>
+                <p className='my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-400'>
+                  ailor you landing page's look and feel from the color scheme to the font size, to design of the page
+                </p>
+              </div>
 
+            </div>
+          </div>
       </div>
     </div>
   );
